be471b9f8bb0a4931a8234790ccb9890e9261cfe
Kyle Clark <kyle@Kyles-MacBook-Air.local>
2021-06-28 01:49:04 +1200

    initialized exercise object

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//Javascript console script that generates a list of random exercises
+//Javascript console script that generates a list of random exercise
+//Object of exercises organised by body section and muscle
+const exercises = {
+  lowerBody: {
+    glutes: [],
+    quads: [],
+    hamstrings: [],
+    calves: [],
+  },
+  upperBody: {
+    neck: [],
+    traps: [],
+    shoulders: [],
+    chest: [],
+    biceps: [],
+    forearms: [],
+    abs: [],
+    triceps: [],
+    lats: [],
+    middleBack: [],
+    lowerBack: [],
+  },
+}
